05c7d3804f335a7b14e7c579e26cd98f491f8e7d
kerubo <mongaremetrine5@gmail.com>
2026-02-26 19:18:15 +0300

    oop-1

--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    def __init__(self, size, price):
+        self.size = size  
+        self.price = price
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        valid_sizes = ["Small", "Medium", "Large"]
+        if value not in valid_sizes:
+            print("size must be Small, Medium, or Large")
+        else:
+            self._size = value
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
+    #pass
